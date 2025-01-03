9fbcd777479a79b5f1e161f69d99603190d3f63a
Sawyer Eckberg <sawyereckberg7@gmail.com>
2025-01-03 17:25:22 +0000

    Tutorial changes 2

---
+++ student_info.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Info about my computer science students from students database
+
+echo -e "\n~~ My Computer Science Students ~~\n"
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:\n"
