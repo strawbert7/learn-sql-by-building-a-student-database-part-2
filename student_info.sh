530941280e0522c48201994d196fd703dd3b6d99
Sawyer Eckberg <sawyereckberg7@gmail.com>
2025-01-03 18:21:40 +0000

    Save progress: 1300.1

--- student_info.sh
+++ student_info.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#Info about my computer science students from students database
+
+# Info about my computer science students from students database
 
 echo -e "\n~~ My Computer Science Students ~~\n"
+
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
-echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:\n"
+
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
